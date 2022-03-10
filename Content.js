c46c57f35265f794470c4122a749225e2b59cc06
Ernest Leung <ernest@Ernests-MacBook-Pro.local>
2022-03-09 20:42:16 -0800

    added sound

--- Content.js
+++ Content.js
@@ -1,15 +1,21 @@
 // array to contain images of Regis
 
 const regisArray = [
-    'https://pbs.twimg.com/media/FNdUBCoVIAAtw_3?format=png&name=small',
-    'https://pbs.twimg.com/media/FNdT-SzVUAceo4b?format=png&name=small',
-    'https://pbs.twimg.com/media/FNdT2pFVcAI2wj2?format=png&name=small',
-    'https://pbs.twimg.com/media/FNdT0RWVIAAwTEd?format=png&name=small',
-    'https://pbs.twimg.com/media/FNdTtokVcAA8tUY?format=png&name=small'
-]
+  'https://pbs.twimg.com/media/FNdUBCoVIAAtw_3?format=png&name=small',
+  'https://pbs.twimg.com/media/FNdT-SzVUAceo4b?format=png&name=small',
+  'https://pbs.twimg.com/media/FNdT2pFVcAI2wj2?format=png&name=small',
+  'https://pbs.twimg.com/media/FNdT0RWVIAAwTEd?format=png&name=small',
+  'https://pbs.twimg.com/media/FNdTtokVcAA8tUY?format=png&name=small',
+];
 // variable to store the image tags on the current page
 const pageImages = document.getElementsByTagName('img');
-console.log(pageImages);
+
+// create a sum to store the total Regis Coins earned
+// sum will be calculated by adding each photos width
+
+const webImgCount = pageImages.length;
+
+console.log(pageImages[0].clientWidth);
 
 // iterate through the length of how many images are on the current page
 for (let i = 0; i < pageImages.length; i++) {
@@ -18,3 +24,13 @@ for (let i = 0; i < pageImages.length; i++) {
   //reassign the current images src to the regius URL
   pageImages[i].src = regisArray[regisPhoto];
 }
+
+// Bark when click
+var box = document.querySelector('.box');
+
+var audioUrl = 'https://freewavesamples.com/files/Dog-Bark.wav';
+
+// Detect all click events on the document
+document.addEventListener('click', function (event) {
+  new Audio(audioUrl).play();
+});
